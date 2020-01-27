c39bc039f13dd94050aad050aa6377f2a542f238
Grzegorz Choiński <grzegorzchoinski97@gmail.com>
2020-01-27 13:26:59 +0100

    g

---
+++ Foodie/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Foodie.Data;
+using Foodie.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Foodie.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+        public ProductsController(ApplicationDbContext applicationDbContext)
+        {
+            _dbContext = applicationDbContext;
+        }
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _dbContext.Products.ToList();
+        }
+
+        [HttpPost]
+        public void Add([FromBody] Product product)
+        {
+            var pr = new Product();
+            pr.Name = product.Name;
+            _dbContext.Add(pr);
+            _dbContext.SaveChanges();
+        }
+    }
+}
