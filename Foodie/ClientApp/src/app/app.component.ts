cf94cb56df02b5b1f5b6d416e539d1ebf6ffb832
Grzegorz Choiński <grzegorzchoinski97@gmail.com>
2020-02-06 00:54:07 +0100

    Button animation polish

--- Foodie/ClientApp/src/app/app.component.ts
+++ Foodie/ClientApp/src/app/app.component.ts
@@ -14,31 +14,40 @@ export class AppComponent {
   title = 'app';
 
   constructor(private _router: Router) {
-    
+
   }
-  
+
   onSwipeRight(evt) {
     var x = this._router.routerState.snapshot.url;
-    if(x == '/mealcalendar'){
+    if (x == '/mealcalendar') {
       this._router.navigateByUrl('home');
-    }else if(x == '/home'){
+    } else if (x == '/home') {
       this._router.navigateByUrl('/data')
-    }
+    } else return;
+
+    var list = document.getElementsByClassName("floating-buttons");
+    [].forEach.call(list, function (el) {
+      el.classList.add('invisible');
+    });
   }
 
   onSwipeLeft(evt) {
     var x = this._router.routerState.snapshot.url;
-    if(x == '/home'){
+    if (x == '/home') {
       this._router.navigateByUrl('/mealcalendar');
-    }else if(x=='/data'){
+    } else if (x == '/data') {
       this._router.navigateByUrl('/home');
-    }
+    }else return
+   
+    var list = document.getElementsByClassName("floating-buttons");
+    [].forEach.call(list, function (el) {
+      el.classList.add('invisible');
+    });
   }
 
-  onSwipeDown(evt){
+  onSwipeDown(evt) {
     evt.srcEvent.stopPropagation();
     evt.preventDefault();
-
   }
 
   prepareRoute(outlet: RouterOutlet) {
