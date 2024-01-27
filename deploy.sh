381fa603781d032d803b41204b0e26d234633aae
Grzegorz Choiński <grzegorzchoinski97@gmail.com>
2024-01-27 20:49:47 +0100

    fix
    Signed-off-by: Grzegorz Choiński <grzegorzchoinski97@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 rm -rf .env
-echo $TAG >> .env
-echo $REGISTRY >> .env
-echo $IMAGE_NAME >> .env
-echo $CERT_KEY >> .env
-echo $CERT_FULLCHAIN >> .env
+echo TAG=$TAG >> .env
+echo REGISTRY=$REGISTRY >> .env
+echo IMAGE_NAME=$IMAGE_NAME >> .env
+echo CERT_KEY=$CERT_KEY >> .env
+echo CERT_FULLCHAIN=$CERT_FULLCHAIN >> .env
 
 rm -rf .nginx
 mkdir .nginx
