c082753cc6f99d6540c3befe02fff8e1c758dcc4
Grzegorz Choiński <grzegorzchoinski97@gmail.com>
2024-02-11 00:06:28 +0100

    ui: show app version
    Signed-off-by: Grzegorz Choiński <grzegorzchoinski97@gmail.com>

--- Foodie/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ Foodie/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment'; 
 
 @Component({
   selector: 'app-nav-menu',
@@ -7,6 +8,7 @@ import { Component } from '@angular/core';
 })
 export class NavMenuComponent {
   isExpanded = false;
+  version = environment.version;
 
   collapse() {
     this.isExpanded = false;
